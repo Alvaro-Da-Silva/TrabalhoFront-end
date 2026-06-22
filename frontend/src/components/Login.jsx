de91ad6a00e5f21c1c97dae34c9be62a461f69cd
Alvaro Da Silva <133697238+Alvaro-Da-Silva@users.noreply.github.com>
2026-06-22 10:43:20 -0300

    Organização de arquivos e diretórios

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { useFetchUser } from "../Hooks/useFetchUser";
+import { useNavigate } from "react-router-dom";
+
+export default function LoginComponent() {
+  const { data: Datausers } = useFetchUser("http://localhost:3000/usuarios");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!Datausers || !Array.isArray(Datausers)) {
+      return <p>Carregando...</p>
+    }
+
+    const usuarioEncontrado = Datausers.find(
+      (user) => user.email === email && user.senha === password,
+    );
+
+    if (usuarioEncontrado) {
+      localStorage.setItem("userId", usuarioEncontrado.id);
+      navigate("/Orders");
+    } else {
+      console.log("Email ou senha inválidos.");
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          placeholder="Email"
+        />
+        <label htmlFor="password">Senha</label>
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Senha"
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
